b826fb58cdf9ad18a47c1672134b527e82e6a7ea
MinexAutomation <minex.automation@gmail.com>
2020-02-20 01:21:43 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Suebia.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.Suebia.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="DefaultSecretsFilePathProvider"/> implementation of <see cref="ISecretsFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultSecretsFilePathProvider(this IServiceCollection services,
+            ServiceAction<ISecretsDirectoryPathProvider> addSecretsDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<ISecretsFilePathProvider, DefaultSecretsFilePathProvider>()
+                .RunServiceAction(addSecretsDirectoryPathProvider)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DefaultSecretsFilePathProvider"/> implementation of <see cref="ISecretsFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ISecretsFilePathProvider> AddDefaultSecretsFilePathProviderAction(this IServiceCollection services,
+            ServiceAction<ISecretsDirectoryPathProvider> addSecretsDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<ISecretsFilePathProvider>(() => services.AddDefaultSecretsFilePathProvider(
+                addSecretsDirectoryPathProvider,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
