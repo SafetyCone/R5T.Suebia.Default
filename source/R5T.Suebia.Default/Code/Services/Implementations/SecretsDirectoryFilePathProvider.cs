c206f4fe612dcdc5d5f1db11ccf916ca8ed39f2c
MinexAutomation <minex.automation@gmail.com>
2020-06-08 15:10:58 -0700

    Changed name of SecretsDirectoryFilePathProvider service by removing "Default-" name prefix segment.

--- source/R5T.Suebia.Default/Code/Services/Implementations/SecretsDirectoryFilePathProvider.cs
+++ source/R5T.Suebia.Default/Code/Services/Implementations/SecretsDirectoryFilePathProvider.cs
@@ -9,13 +9,13 @@ namespace R5T.Suebia.Default
     /// Provides secrets file paths using a <see cref="ISecretsDirectoryPathProvider"/> service.
     /// Assumes that secrets files are in the secrets directory directly.
     /// </summary>
-    public class DefaultSecretsDirectoryFilePathProvider : ISecretsDirectoryFilePathProvider
+    public class SecretsDirectoryFilePathProvider : ISecretsDirectoryFilePathProvider
     {
         public ISecretsDirectoryPathProvider SecretsDirectoryPathProvider { get; }
         public IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
 
 
-        public DefaultSecretsDirectoryFilePathProvider(ISecretsDirectoryPathProvider secretsDirectoryPathProvider, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        public SecretsDirectoryFilePathProvider(ISecretsDirectoryPathProvider secretsDirectoryPathProvider, IStringlyTypedPathOperator stringlyTypedPathOperator)
         {
             this.SecretsDirectoryPathProvider = secretsDirectoryPathProvider;
             this.StringlyTypedPathOperator = stringlyTypedPathOperator;
